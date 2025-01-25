601dec67df6ee3e7a2ebce31861c7e8ae62a0e0b
Hasnatove <hasnatovi850@gmail.com>
2025-01-26 02:15:23 +0600

    login page and signup page design
    login page and signup page design

--- MovieTicketBookingManagementSystem/src/movieticketbookingmanagementsystem/FXMLDocumentController.java
+++ MovieTicketBookingManagementSystem/src/movieticketbookingmanagementsystem/FXMLDocumentController.java
@@ -9,7 +9,12 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 /**
  *
@@ -18,13 +23,51 @@ import javafx.scene.control.Label;
 public class FXMLDocumentController implements Initializable {
     
     @FXML
-    private Label label;
-    
+    private Button signUp_Btn;
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    private Hyperlink signUp_alreadyHaveAccount;
+
+    @FXML
+    private Button signUp_close;
+
+    @FXML
+    private TextField signUp_email;
+
+    @FXML
+    private AnchorPane signUp_form;
+
+    @FXML
+    private FontAwesomeIconView signUp_minimize;
+
+    @FXML
+    private PasswordField signUp_password;
+
+    @FXML
+    private TextField signUp_username;
+
+    @FXML
+    private FontAwesomeIconView signin_close;
+
+    @FXML
+    private Hyperlink signin_createAccount;
+
+    @FXML
+    private AnchorPane signin_form;
+
+    @FXML
+    private Button signin_loginBtn;
+
+    @FXML
+    private FontAwesomeIconView signin_minimize;
+
+    @FXML
+    private PasswordField signin_password;
+
+    @FXML
+    private TextField signin_username;
+
+
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
